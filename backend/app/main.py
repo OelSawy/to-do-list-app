5bbfdd7f2d272daea4597ae4abeeeb983463cdba
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2025-08-18 11:14:44 +0000

    misc

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from config.db import connect_db
+from routes.user import router as user_router
+from routes.task import router as task_router
 
 app = FastAPI();
 
-@app.get("/")
-def index():
-    return {"message": "Hello, World!"}
+@app.on_event("startup")
+async def init_db():
+    connect_db()
+
+app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(task_router, prefix="/tasks", tags=["Tasks"])
